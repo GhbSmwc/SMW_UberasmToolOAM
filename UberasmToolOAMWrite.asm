8bcf45f0b26d5c743d7d30dcc24a8f908b735078
GhbSmwc <GhbSmwc@users.noreply.github.com>
2019-03-09 22:52:48 -0500

    Add files via upload
    Optimized the Y position to no longer use a slot of all parts of the sprite is offscreen.

--- UberasmToolOAMWrite.asm
+++ UberasmToolOAMWrite.asm
@@ -34,14 +34,18 @@ WriteOAM:
 	RTL
 	
 	.SlotFound
-	
 	..YPosHandle
-	REP #$20			;\Don't take up a slot of offscreen vertically.
-	LDA $02				;|
-	CMP #$00E0			;|
-	BCC .HandleXPos			;|
-	CMP #$FFF1			;|
-	BCS .HandleXPos			;/
+	print "-----------------------------------",pc
+	REP #$20
+	LDA $06					;\Position #$FFF8 is invisible for 8x8 tiles and
+	ASL					;|Position #$FFF0 is invisible for 16x16 tiles.
+	AND #$00FF				;/
+	TAX
+	LDA $02					;\Don't take up a slot of offscreen vertically.
+	CMP #$00E0				;|
+	BCC .HandleXPos				;|
+	CMP.l TopLeftScreenBoundaryPos,x	;|
+	BCS .HandleXPos				;/
 
 	.OffScreen
 	SEP #$20
@@ -56,14 +60,11 @@ WriteOAM:
 	STA $0201|!addr,y		;/
 	
 	..XPositionHandle
-	LDA $06					;\Position #$FFF8 is invisible for 8x8 tiles and
-	ASL					;|Position #$FFF0 is invisible for 16x16 tiles.
-	TAX					;/
 	REP #$20
 	LDA $00					;\Determine if on-screen horizontally
 	CMP #$0100				;|
 	BCC ..OnScreenHoriz			;|
-	CMP LeftScreenBoundaryPos,x		;|
+	CMP.l TopLeftScreenBoundaryPos,x		;|
 	BCS ..OnScreenHoriz			;/
 	BRA .OffScreen				;>If not skip the whole thing.
 	
@@ -72,7 +73,6 @@ WriteOAM:
 	LDA $00					;\X position bits 0-7
 	STA $0200|!addr,y			;/
 	
-	print "---------------------------------",pc
 	REP #$20
 	TYA			;>Take index
 	LSR #2			;>Divide by 4 to obtain slot number
@@ -139,7 +139,7 @@ WriteOAM:
 	CLC
 	RTL
 	
-	LeftScreenBoundaryPos:
+	TopLeftScreenBoundaryPos:
 	dw $FFF9, $FFF1
 
 	BitTableXHigh:
